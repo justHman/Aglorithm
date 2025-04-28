bdd6af68688ba36b9d272eb8e8515189d9c98943
justHman <hmantools11@gmail.com>
2025-04-28 22:42:25 +0700

    up

--- test.py
+++ test.py
@@ -1,30 +1,18 @@
-def longest_palindrome(s):
-    if not s:
-        return ""
 
-    def expand_around_center(left, right):
-        while left > 0 and right < len(s) and s[left] == s[right]:
-            print(s[left], s[right])
-            left -= 1
-            right += 1
-        # Trả về chuỗi đối xứng tìm được (bỏ đi ký tự thừa cuối cùng)
-        print(s[left:right])
-        return s[left:right]
+def my_permutation(nums):
+    def back_track(nums, so = []):
+        if len(so) == 3:
+            print(''.join(so))
+            return 
+        
+        for num in nums:
+            so.append(str(num))
+            a = [v for v in nums if v != num]
+            back_track(a, so)
+            so.pop()
+    back_track(nums)
+            
 
-    longest = ""
-    for i in range(len(s)):
-        # Trường hợp tâm là 1 ký tự
-        odd_pal = expand_around_center(i, i)
-        # Trường hợp tâm là 2 ký tự
-        even_pal = expand_around_center(i, i + 1)
+nums = [1, 2, 3]
+my_permutation(nums)
 
-        # Lấy chuỗi đối xứng dài hơn
-        longest = max(longest, odd_pal, even_pal, key=len)
-
-    return longest
-
-
-
-if __name__ == "__main__":
-    s = "cbbd"
-    print(longest_palindrome(s))
