39fb8d8db27f0e068775c7f96b75f7053285afe3
justHman <hmantools11@gmail.com>
2025-05-01 23:56:24 +0700

    up

--- test.py
+++ test.py
@@ -1,4 +1,25 @@
-a= ''
-a += '1'
-print(a)
-print()
+def totalNumbers(digits) -> int:
+        evens = 0
+        def permutate(digits, nums = []):
+            nonlocal evens
+            if len(nums) == 3:
+                number = int(''.join(nums))
+                if number % 2 == 0 and len(str(number)) == 3:
+                    evens += 1
+                return 
+            
+            for digit in digits:
+                nums.append(str(digit))
+                flt_digits = []
+                c = 0
+                for v in digits:
+                    if v == digit and c != 1:
+                        c = 1
+                        continue
+                    flt_digits.append(v)
+                permutate(flt_digits, nums)
+                nums.pop()
+        permutate(digits)
+        return evens
+
+totalNumbers([0, 2, 2])
