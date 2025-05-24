96dcfb8e0572e0583b9425049f350de8fcd4ec72
justHman <hmantools11@gmail.com>
2025-05-24 21:12:34 +0700

    ip

--- practice.py
+++ practice.py
@@ -1,15 +1,15 @@
 def s(a, t):
-    l,r=0, len(a) - 1
+    l,r = 0, len(a) - 1
     while l <= r:
-        m = (r+l)//2
+        m = (l+r) // 2
         if t == a[m]:
             return m
-        elif t<a[m]:
+        elif t < a[m]:
             r = m - 1
         else:
-            l = m +1
+            l = m + 1
     return -1
 
-a = [1,3,5,7,9]
-rs = s(a, 1)
-print(rs)
+a = [1, 2, 6, 9]
+for t in [0, 1, 2, 5, 8, 9, 10]:
+    print(s(a,t))
